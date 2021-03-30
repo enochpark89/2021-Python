8cf477105a6ba6947e8d832d69351af8a2039050
enochpark89 <44038883+enochpark89@users.noreply.github.com>
2021-03-29 21:07:56 -0700

    20210329-CodeChallenge1Answer
    CodeChallenge1Answer updated!

---
+++ 20210329-NomadCoder-Challenge1/1.4-1.8.py
@@ -0,0 +1,15 @@
+"""
+# 1.4. Create a function -> Python: Define a function
+
+# Define the function; make sure to indent body.
+def say_hello():
+    print("Hello")
+
+
+# Call a function as below
+say_hello()
+
+# 1.5 Function Arguments
+def say_hello():
+    print("Hello")
+"""
